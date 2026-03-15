572436b55a9a1301a972657366f3a13f26b6f890
Noah Stefano <noahba157@gmail.com>
2026-03-15 21:09:08 +0000

    Changed mock data strings

--- src/mockRadioFeed.ts
+++ src/mockRadioFeed.ts
@@ -1,9 +1,9 @@
 import type { RadioMessage } from './types'
 
 const mockEntries = [
-  { id: 12, name: 'Control Alpha', self: false },
-  { id: 27, name: 'Sierra Two-One', self: true },
-  { id: 34, name: 'Fire Ops Three', self: false },
+  { id: 12, name: 'AW-798', self: false },
+  { id: 27, name: 'OE-075 | 175BX', self: true },
+  { id: 34, name: 'HELIMED 27', self: false },
 ]
 
 const dispatchRadioMessage = (message: RadioMessage) => {
@@ -19,7 +19,7 @@ export const startMockRadioFeed = () => {
       radioName: entry.name,
       self: entry.self,
       channel: 'PAN LONDON',
-      channelFrequency: 41,
+      channelFrequency: 10,
     })
   }
 
